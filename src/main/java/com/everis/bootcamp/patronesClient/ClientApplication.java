704508fd8a570fe70beab8046c7086b347d7f10d
rafliefue <rafliefue@gmail.com>
2021-11-17 11:38:20 +0100

    RETO METRICAS CLIENT

--- src/main/java/com/everis/bootcamp/patronesClient/ClientApplication.java
+++ src/main/java/com/everis/bootcamp/patronesClient/ClientApplication.java
@@ -1,32 +1,41 @@
 package com.everis.bootcamp.patronesClient;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 public class ClientApplication implements CommandLineRunner {
 
+	@Autowired
+	EndpointEstados a;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
 	
 	public void run(String... args) throws Exception{
 		
+		/**Aqui lo que hago es el circuit breaker y las escrituras de los
+		estados por los que pasa en el end point**/
+		
 		StatusService status = new StatusService();
-		Coche coche = new Coche.CocheBuilder("open").build();
-	
-		for(int i=0; i <= 5 && coche.getStatus().equals("open"); i++) {
-			
-			coche.setStatus("half-open");
+		
+		for(int i=0; i <= 5; i++) {
+			a.writeOperation("half-open");
 			status.halfOpen();
 			Thread.sleep(3000);
 			if(i >= 3) {
-				coche.setStatus("close");
+				a.writeOperation("close");
 				status.close();
 				Thread.sleep(3000);
 			} else {
-				coche.setStatus("open");
+				a.writeOperation("open");
 				status.close();
 				Thread.sleep(3000);
 			}
--- src/main/java/com/everis/bootcamp/patronesClient/StatusService.java
+++ src/main/java/com/everis/bootcamp/patronesClient/StatusService.java
@@ -4,11 +4,12 @@ import java.net.URI;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 public class StatusService{
@@ -20,24 +21,21 @@ public class StatusService{
 	String url = "http://localhost:8888/";
 	
 	public String open() {
-		String resourceUrl = url + "open";
-		
+		String resourceUrl = url + "open";	
 		ResponseEntity<String> res = rtplate.getForEntity(URI.create(resourceUrl), String.class);
 		logger.info("ESTADO: OPEN");
 		return res.getBody();
 	}
 	
 	public String close() {
-		String resourceUrl = url + "closed";
-		
+		String resourceUrl = url + "closed";	
 		ResponseEntity<String> res = rtplate.getForEntity(URI.create(resourceUrl), String.class);
 		logger.info("ESTADO: CLOSED");
 		return res.getBody();
 	}
 	
 	public String halfOpen() {
-		String resourceUrl = url + "half-open";
-		
+		String resourceUrl = url + "half-open";	
 		ResponseEntity<String> res = rtplate.getForEntity(URI.create(resourceUrl), String.class);
 		logger.info("ESTADO: HALF OPEN");
 		return res.getBody();
